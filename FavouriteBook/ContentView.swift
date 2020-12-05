e33e512948246f3fa806b21d2aa2f30c32e127f0
owner <canerduru@ymail.com>
2020-12-05 19:53:18 +0300

    Initial Commit

---
+++ FavouriteBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavouriteBook
+//
+//  Created by owner on 12/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
